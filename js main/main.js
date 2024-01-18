60c000dad872cd496191572f253582db01986595
DemetrioPadre <demetriopadre@gmail.com>
2024-01-18 17:29:37 +0100

    prove

--- js main/main.js
+++ js main/main.js
@@ -1,9 +1,16 @@
 console.log('ciao padrone');
 
+//costante che collega le box da html a js 
+const boxes = document.querySelector('.box');
+
 
 // scriviamo un ciclo che stampi i numeri da 1 a 100
 
 for (let i = 1; i <= 100; i++) {
     let result;
-    console.log(i);
+    result = i;
+    const box = `<div class="boxes">${result}</div>`;
+    boxes.innerHTML += box;
+
+
 }
