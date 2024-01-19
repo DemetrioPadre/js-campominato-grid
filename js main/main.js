f42fec698cb85e8971be06160711d29062863ab6
DemetrioPadre <demetriopadre@gmail.com>
2024-01-19 17:12:51 +0100

    add-box d flex

--- js main/main.js
+++ js main/main.js
@@ -1,16 +1,20 @@
 console.log('ciao padrone');
 
 //costante che collega le box da html a js 
-const boxes = document.querySelector('.box');
+const boxes = document.getElementById('gameBox');
 
 
 // scriviamo un ciclo che stampi i numeri da 1 a 100
 
 for (let i = 1; i <= 100; i++) {
-    let result;
-    result = i;
-    const box = `<div class="boxes">${result}</div>`;
-    boxes.innerHTML += box;
-
+    // let result;
+    // result = i;
+    // const scatola = `<div class="box ">${i}</div>`;
+    // boxes.innerHTML += scatola;
+    // console.log(i);
+    const cardElement = document.createElement('div');
+    cardElement.classList.add('box');
+    cardElement.innerHTML = i;
+    boxes.append(cardElement);
 
 }
